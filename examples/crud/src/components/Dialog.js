452366450891b04b59e1dd165f4ef13fc055a7e0
Shivani Sharma <s.sharma@celestialsys.com>
2020-09-16 20:23:26 +0530

    LJS-75 converted recompose to hooks in Dialog.js, ListActions.js, Item.js files to remove React.createFactory warning in console

--- examples/crud/src/components/Dialog.js
+++ examples/crud/src/components/Dialog.js
@@ -1,142 +1,137 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { compose, withHandlers, defaultProps } from 'recompose';
-import { inject, observer } from 'mobx-react';
-import { withStyles } from '@material-ui/core';
-
-const enhanceConfirmation = compose(
-  inject('uiStore'),
-  defaultProps({
-    title: 'Confirm delete',
-    content: 'Are you sure to delete this item?',
-    onClose: () => null,
-    onAccept: () => null,
-    onCancel: () => null,
-    acceptText: 'Confirm',
-    cancelText: 'Cancel',
-  }),
-  withHandlers({
-    close: ({ uiStore, id, onClose }) => () => {
-      onClose();
-      uiStore.dialogs.close(id);
-    },
-  }),
-  withHandlers({
-    accept: ({ close, onAccept }) => () => {
-      onAccept();
-      close();
-    },
-    cancel: ({ close, onCancel }) => () => {
-      onCancel();
-      close();
-    },
-  }),
-  withStyles((theme) => ({
-    paper: {
-      borderBottomStyle: 'solid',
-      borderBottomColor: theme.palette.primary[theme.palette.type],
-      borderBottomWidth: theme.spacing(0.5),
-    },
-  })),
-  observer
-);
-
-export const ConfirmationDialog = enhanceConfirmation(
-  ({ id, title, content, close, accept, cancel, acceptText, cancelText, classes }) => (
-    <Dialog onClose={close} aria-labelledby={id} open={true} classes={classes}>
-      <DialogTitle id={id}>{title}</DialogTitle>
-      <DialogContent>
-        <DialogContentText>{content}</DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={cancel} color="default" variant="contained" autoFocus>
-          {cancelText}
-        </Button>
-        <Button onClick={accept} color="primary" variant="contained">
-          {acceptText}
-        </Button>
-      </DialogActions>
-    </Dialog>
-  )
-);
-
-// import React, {Component} from 'react';
+// import React from 'react';
 // import Button from '@material-ui/core/Button';
 // import Dialog from '@material-ui/core/Dialog';
 // import DialogActions from '@material-ui/core/DialogActions';
 // import DialogContent from '@material-ui/core/DialogContent';
 // import DialogContentText from '@material-ui/core/DialogContentText';
 // import DialogTitle from '@material-ui/core/DialogTitle';
+// import { compose, withHandlers, defaultProps } from 'recompose';
 // import { inject, observer } from 'mobx-react';
 // import { withStyles } from '@material-ui/core';
 
-// const styles = (theme) => ({
-//   paper: {
-//     borderBottomStyle: 'solid',
-//     borderBottomColor: theme.palette.primary[theme.palette.type],
-//     borderBottomWidth: theme.spacing(0.5),
-//   },
-// });
+// const enhanceConfirmation = compose(
+//   inject('uiStore'),
+//   defaultProps({
+//     title: 'Confirm delete',
+//     content: 'Are you sure to delete this item?',
+//     onClose: () => null,
+//     onAccept: () => null,
+//     onCancel: () => null,
+//     acceptText: 'Confirm',
+//     cancelText: 'Cancel',
+//   }),
+//   withHandlers({
+//     close: ({ uiStore, id, onClose }) => () => {
+//       onClose();
+//       uiStore.dialogs.close(id);
+//     },
+//   }),
+//   withHandlers({
+//     accept: ({ close, onAccept }) => () => {
+//       onAccept();
+//       close();
+//     },
+//     cancel: ({ close, onCancel }) => () => {
+//       onCancel();
+//       close();
+//     },
+//   }),
+//   withStyles((theme) => ({
+//     paper: {
+//       borderBottomStyle: 'solid',
+//       borderBottomColor: theme.palette.primary[theme.palette.type],
+//       borderBottomWidth: theme.spacing(0.5),
+//     },
+//   })),
+//   observer
+// );
 
-// class ConfirmDialog extends Component {
-
-//   close = ( uiStore, id, onClose ) => () => {
-//     console.log("hahaha",uiStore.projectList);
-//     onClose();
-//     uiStore.dialogs.close(id);
-//   }
-
-//   accept = ( onAccept, uiStore, id, onClose ) => () => {
-//     onAccept();
-//     this.close(uiStore, id, onClose);
-//   }
-//   cancel = ( onCancel, uiStore, id, onClose ) => () => {
-//     onCancel();
-//     this.close(uiStore, id, onClose);
-//   }
-
-//   render() {
-//     const { id, title, content, onClose, onAccept, onCancel, acceptText, cancelText, classes, uiStore } = this.props;
-//     console.log("PROPS",this.props);
-//     return(
-//       <Dialog onClose={this.close(uiStore,id,onClose)} aria-labelledby={id} open={true} classes={classes}>
-//         <DialogTitle id={id}>{title}</DialogTitle>
-//         <DialogContent>
-//           <DialogContentText>{content}</DialogContentText>
-//         </DialogContent>
-//         <DialogActions>
-//           <Button onClick={this.cancel(onCancel, uiStore, id, onClose)} color="default" variant="contained" autoFocus>
-//             {cancelText}
-//           </Button>
-//           <Button onClick={this.accept(onAccept,uiStore,id,onClose)} color="primary" variant="contained">
-//             {acceptText}
-//           </Button>
-//         </DialogActions>
-//       </Dialog>
-//     )
-//   }
-// }
-
-// ConfirmDialog.defaultProps = {
-//   title: 'Confirm delete',
-//   content: 'Are you sure to delete this item?',
-//   onClose: () => null,
-//   onAccept: () => null,
-//   onCancel: () => null,
-//   acceptText: 'Confirm',
-//   cancelText: 'Cancel'
-// };
-
-// const ConfirmationDialog = withStyles(styles)(ConfirmDialog)
-// export default inject('uiStore')(observer(ConfirmationDialog));
-
-// = enhanceConfirmation(
+// export const ConfirmationDialog = enhanceConfirmation(
 //   ({ id, title, content, close, accept, cancel, acceptText, cancelText, classes }) => (
-
+//     <Dialog onClose={close} aria-labelledby={id} open={true} classes={classes}>
+//       <DialogTitle id={id}>{title}</DialogTitle>
+//       <DialogContent>
+//         <DialogContentText>{content}</DialogContentText>
+//       </DialogContent>
+//       <DialogActions>
+//         <Button onClick={cancel} color="default" variant="contained" autoFocus>
+//           {cancelText}
+//         </Button>
+//         <Button onClick={accept} color="primary" variant="contained">
+//           {acceptText}
+//         </Button>
+//       </DialogActions>
+//     </Dialog>
 //   )
 // );
+
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { inject, observer } from 'mobx-react';
+import { withStyles } from '@material-ui/core';
+
+const styles = (theme) => ({
+  paper: {
+    borderBottomStyle: 'solid',
+    borderBottomColor: theme.palette.primary[theme.palette.type],
+    borderBottomWidth: theme.spacing(0.5),
+  },
+});
+
+class ConfirmDialog extends Component {
+  close = (uiStore, id, onClose) => () => {
+    onClose();
+    uiStore.dialogs.close(id);
+  };
+
+  accept = (uiStore, id, onClose, onAccept) => () => {
+    onAccept();
+    onClose();
+    uiStore.dialogs.close(id);
+  };
+
+  cancel = (uiStore, id, onClose, onCancel) => () => {
+    onCancel();
+    onClose();
+    uiStore.dialogs.close(id);
+  };
+
+  render() {
+    const { id, title, content, onClose, onAccept, onCancel, acceptText, cancelText, classes, uiStore } = this.props;
+    console.log('PROPS', onAccept);
+    return (
+      <Dialog onClose={this.close(uiStore, id, onClose)} aria-labelledby={id} open={true} classes={classes}>
+        <DialogTitle id={id}>{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{content}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.cancel(uiStore, id, onClose, onCancel)} color="default" variant="contained" autoFocus>
+            {cancelText}
+          </Button>
+          <Button onClick={this.accept(uiStore, id, onClose, onAccept)} color="primary" variant="contained">
+            {acceptText}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+ConfirmDialog.defaultProps = {
+  title: 'Confirm delete',
+  content: 'Are you sure to delete this item?',
+  onClose: () => null,
+  onAccept: () => null,
+  onCancel: () => null,
+  acceptText: 'Confirm',
+  cancelText: 'Cancel',
+};
+
+const ConfirmationDialog = withStyles(styles)(ConfirmDialog);
+export default inject('uiStore')(observer(ConfirmationDialog));
--- examples/crud/src/components/List/ListActions.js
+++ examples/crud/src/components/List/ListActions.js
@@ -1,5 +1,89 @@
-import React from 'react';
-import { compose, withHandlers } from 'recompose';
+// import React from 'react';
+// import { compose, withHandlers } from 'recompose';
+// import { Grid, Button } from '@material-ui/core';
+// import SelectAllButton from './SelectAllButton';
+// import SelectionInfo from './SelectionInfo';
+// import Filter from './Filter';
+// import Sort from './Sort';
+// import AddButton from './AddButton';
+
+// const enhanceListActions = compose(
+//   withHandlers({
+//     onChange: ({ selectAllOnSelectAll, selectAllOnUnselectAll }) => (e, checked) =>
+//       checked ? selectAllOnSelectAll() : selectAllOnUnselectAll(),
+//   })
+// );
+
+// export default enhanceListActions(
+//   ({
+//     addButtonTitle,
+//     onAdd,
+//     onChange,
+//     selectAllDisabled,
+//     selectAllChecked,
+//     selectedItems,
+//     selectionOnDelete,
+//     selectionOnActivate,
+//     selectionOnDeactivate,
+//     sortItems,
+//     sortOrder,
+//     sortProperty,
+//     sortOrderOnChange,
+//     sortPropertyOnChange,
+//     filterOnChange,
+//   }) => {
+//     return (
+//       <Grid container direction="row" justify="flex-start" alignItems="center">
+//         <Grid item>
+//           <SelectAllButton onChange={onChange} checked={selectAllChecked} disabled={selectAllDisabled} />
+//         </Grid>
+//         <Grid item xs={4} container alignItems="center" spacing={2}>
+//           {selectedItems > 0 && (
+//             <React.Fragment>
+//               <Grid item xs={3} container justify="center">
+//                 <SelectionInfo selectedCount={selectedItems} />
+//               </Grid>
+//               <Grid item container spacing={1} xs>
+//                 <Grid item>
+//                   <Button variant="outlined" color="secondary" onClick={selectionOnDelete}>
+//                     Delete
+//                   </Button>
+//                 </Grid>
+//                 <Grid item>
+//                   <Button variant="outlined" color="primary" onClick={selectionOnActivate}>
+//                     Activate
+//                   </Button>
+//                 </Grid>
+//                 <Grid item>
+//                   <Button variant="outlined" onClick={selectionOnDeactivate}>
+//                     Deactivate
+//                   </Button>
+//                 </Grid>
+//               </Grid>
+//             </React.Fragment>
+//           )}
+//         </Grid>
+//         <Grid item xs container justify="center">
+//           <Filter onChange={filterOnChange} />
+//         </Grid>
+//         <Grid item xs={3} container justify="flex-end">
+//           <Sort
+//             sortItems={sortItems}
+//             sortProperty={sortProperty}
+//             sortOrder={sortOrder}
+//             sortOrderOnChange={sortOrderOnChange}
+//             sortPropertyOnChange={sortPropertyOnChange}
+//           />
+//         </Grid>
+//         <Grid item>
+//           <AddButton onClick={onAdd}>{addButtonTitle}</AddButton>
+//         </Grid>
+//       </Grid>
+//     );
+//   }
+// );
+
+import React, { Component } from 'react';
 import { Grid, Button } from '@material-ui/core';
 import SelectAllButton from './SelectAllButton';
 import SelectionInfo from './SelectionInfo';
@@ -7,35 +91,39 @@ import Filter from './Filter';
 import Sort from './Sort';
 import AddButton from './AddButton';
 
-const enhanceListActions = compose(
-  withHandlers({
-    onChange: ({ selectAllOnSelectAll, selectAllOnUnselectAll }) => (e, checked) =>
-      checked ? selectAllOnSelectAll() : selectAllOnUnselectAll(),
-  })
-);
+class ListActions extends Component {
+  onChange = (selectAllOnSelectAll, selectAllOnUnselectAll) => (e, checked) => {
+    checked ? selectAllOnSelectAll() : selectAllOnUnselectAll();
+  };
 
-export default enhanceListActions(
-  ({
-    addButtonTitle,
-    onAdd,
-    onChange,
-    selectAllDisabled,
-    selectAllChecked,
-    selectedItems,
-    selectionOnDelete,
-    selectionOnActivate,
-    selectionOnDeactivate,
-    sortItems,
-    sortOrder,
-    sortProperty,
-    sortOrderOnChange,
-    sortPropertyOnChange,
-    filterOnChange,
-  }) => {
+  render() {
+    const {
+      addButtonTitle,
+      onAdd,
+      selectAllDisabled,
+      selectAllChecked,
+      selectedItems,
+      selectionOnDelete,
+      selectionOnActivate,
+      selectionOnDeactivate,
+      sortItems,
+      sortOrder,
+      sortProperty,
+      sortOrderOnChange,
+      sortPropertyOnChange,
+      filterOnChange,
+      selectAllOnSelectAll,
+      selectAllOnUnselectAll,
+    } = this.props;
+
     return (
       <Grid container direction="row" justify="flex-start" alignItems="center">
         <Grid item>
-          <SelectAllButton onChange={onChange} checked={selectAllChecked} disabled={selectAllDisabled} />
+          <SelectAllButton
+            onChange={this.onChange(selectAllOnSelectAll, selectAllOnUnselectAll)}
+            checked={selectAllChecked}
+            disabled={selectAllDisabled}
+          />
         </Grid>
         <Grid item xs={4} container alignItems="center" spacing={2}>
           {selectedItems > 0 && (
@@ -81,96 +169,6 @@ export default enhanceListActions(
       </Grid>
     );
   }
-);
+}
 
-// eslint-disable-next-line prettier/prettier
-
-// import React, { Component } from 'react';
-// import { Grid, Button } from '@material-ui/core';
-// import SelectAllButton from './SelectAllButton';
-// import SelectionInfo from './SelectionInfo';
-// import Filter from './Filter';
-// import Sort from './Sort';
-// import AddButton from './AddButton';
-
-// eslint-disable-next-line prettier/prettier
-
-// class ListActions extends Component {
-
-//   onChange = ( selectAllOnSelectAll, selectAllOnUnselectAll ) => (e, checked) => {
-//   checked ? selectAllOnSelectAll() : selectAllOnUnselectAll()
-//   }
-
-//   render() {
-//     const {
-//       addButtonTitle,
-//       onAdd,
-//       selectAllDisabled,
-//       selectAllChecked,
-//       selectedItems,
-//       selectionOnDelete,
-//       selectionOnActivate,
-//       selectionOnDeactivate,
-//       sortItems,
-//       sortOrder,
-//       sortProperty,
-//       sortOrderOnChange,
-//       sortPropertyOnChange,
-//       filterOnChange,
-//       selectAllOnSelectAll,
-//       selectAllOnUnselectAll
-//     } = this.props;
-
-//       return (
-//         <Grid container direction="row" justify="flex-start" alignItems="center">
-//           <Grid item>
-//             <SelectAllButton onChange={this.onChange(selectAllOnSelectAll,selectAllOnUnselectAll)} checked={selectAllChecked} disabled={selectAllDisabled} />
-//           </Grid>
-//           <Grid item xs={4} container alignItems="center" spacing={2}>
-//             {selectedItems > 0 && (
-//               <React.Fragment>
-//                 <Grid item xs={3} container justify="center">
-//                   <SelectionInfo selectedCount={selectedItems} />
-//                 </Grid>
-//                 <Grid item container spacing={1} xs>
-//                   <Grid item>
-//                     <Button variant="outlined" color="secondary" onClick={selectionOnDelete}>
-//                       Delete
-//                     </Button>
-//                   </Grid>
-//                   <Grid item>
-//                     <Button variant="outlined" color="primary" onClick={selectionOnActivate}>
-//                       Activate
-//                     </Button>
-//                   </Grid>
-//                   <Grid item>
-//                     <Button variant="outlined" onClick={selectionOnDeactivate}>
-//                       Deactivate
-//                     </Button>
-//                   </Grid>
-//                 </Grid>
-//               </React.Fragment>
-//             )}
-//           </Grid>
-//           <Grid item xs container justify="center">
-//             <Filter onChange={filterOnChange} />
-//           </Grid>
-//           <Grid item xs={3} container justify="flex-end">
-//             <Sort
-//               sortItems={sortItems}
-//               sortProperty={sortProperty}
-//               sortOrder={sortOrder}
-//               sortOrderOnChange={sortOrderOnChange}
-//               sortPropertyOnChange={sortPropertyOnChange}
-//             />
-//           </Grid>
-//           <Grid item>
-//             <AddButton onClick={onAdd}>{addButtonTitle}</AddButton>
-//           </Grid>
-//         </Grid>
-//       );
-//     }
-//   };
-
-// eslint-disable-next-line prettier/prettier
-// export default ListActions;
+export default ListActions;
